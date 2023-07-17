7cdc3416678bbed4ae2ce85b1c0464a00451b727
Joni Nikki <72250615+j-nikki@users.noreply.github.com>
2023-07-17 13:01:45 +0300

    Fix data duplication

--- vocabularium/__main__.py
+++ vocabularium/__main__.py
@@ -5,39 +5,52 @@ from pathlib import Path
 import sqlite3
 from argparse import ArgumentParser
 import sys
-from time import time_ns
-from typing import Any
+from typing import Any, Callable
 
 import pkg_resources
 
-cachedir = os.getenv('XDG_DATA_HOME', os.path.expanduser('~/.local/share')) + '/vocabularium'
-if not os.path.isdir(cachedir):
-    os.makedirs(cachedir, exist_ok=True)
+_cachedir = os.getenv('XDG_DATA_HOME', os.path.expanduser('~/.local/share')) + '/vocabularium'
+if not os.path.isdir(_cachedir):
+    os.makedirs(_cachedir, exist_ok=True)
 
-ap = ArgumentParser('vocabularium', description='A program serving an HTTP API for a vocabulary database.', epilog='The database is stored in a SQLite file. The default location is "$XDG_DATA_HOME/vocabularium/en.db". The database is created if it does not exist.')
-ap.add_argument('--database', '-db', type=Path, default=f'{cachedir}/en.db')
-ap.add_argument('input', nargs='?', type=Path, default=sys.stdin, help='A per-line listing of individual JSON objects, each representing a word. If not specified, read from stdin.')
-args = ap.parse_args()
+_ap = ArgumentParser('vocabularium', description='A program serving an HTTP API for a vocabulary database.', epilog='The database is stored in a SQLite file. The default location is "$XDG_DATA_HOME/vocabularium/en.db". The database is created if it does not exist.')
+_ap.add_argument('--database', '-db', type=Path, default=f'{_cachedir}/en.db')
+_ap.add_argument('--verbose', '-v', action='store_true', help='Print verbose output.')
+_ap.add_argument('input', nargs='?', type=Path, default=sys.stdin, help='A per-line listing of individual JSON objects, each representing a word. If not specified, read from stdin.')
+args = _ap.parse_args()
+
+def _insert(tbl: str, *cols: str, nuniq: int | None = None):
+    stmt = f'insert or ignore into {tbl} ({",".join(cols)}) values ({",".join("?"*len(cols))});'
+    nuniq = nuniq or len(cols)
+    idqry = f'select id from {tbl} where {" and ".join(f"{col}=?" for col in cols[:nuniq])};'
+    def query(cur: sqlite3.Cursor, data: tuple) -> Callable[[], int]:
+        cur.execute(stmt, data)
+        return lambda: cur.execute(idqry, data[:nuniq]).fetchone()[0]
+    return query
+
+_insword = _insert('word', 'lang_code', 'word')
+_insetym = _insert('etymology', 'word_id', 'number', 'text', nuniq=2)
+_instmpl = _insert('etymology_template', 'etymology_id', 'expansion', 'name')
+_inssense = _insert('sense', 'etymology_id', 'wiki_id', 'pos', nuniq=2)
 
 def _insert_word(cur: sqlite3.Cursor, data: Any):
-    cur.execute('insert or ignore into word values (?,?);', (data['lang_code'], data['word']))
-    cur.execute('insert into etymology values (?,?,?,?);', (data['lang_code'], data['word'], data.get('etymology_number'), data.get('text')))
-    eid = cur.lastrowid
+    wid = _insword(cur, (data['lang_code'], data['word']))()
+    eid = _insetym(cur, (wid, data.get('etymology_number', 0), data.get('etymology_text')))()
+    
     for tmpl in data.get('etymology_templates', []):
-        cur.execute('insert into etymology_template values (?,?,?);', (eid, tmpl['expansion'], tmpl['name']))
-        tid = cur.lastrowid
+        tid = _instmpl(cur, (eid, tmpl['expansion'], tmpl['name']))()
         for k, v in tmpl.get('args', {}).items():
-            cur.execute('insert into template_arg values (?,?,?);', (tid, k, v))
+            cur.execute('insert or ignore into template_arg values (?,?,?);', (tid, k, v))
+
     for sense in data.get('senses', []):
-        cur.execute('insert into sense values (?,?);', (eid, data['pos']))
-        sid = cur.lastrowid
+        sid = _inssense(cur, (eid, sense['id'], data['pos']))()
         for gloss in set(sense.get('glosses', [])):
-            cur.execute('insert into sense_gloss values (?,?);', (sid, gloss))
+            cur.execute('insert or ignore into sense_gloss values (?,?);', (sid, gloss))
         for text, type_ in {x['text']: x.get('type') for x in sense.get('examples', [])}.items():
-            cur.execute('insert into sense_example values (?,?,?);', (sid, text, type_))
+            cur.execute('insert or ignore into sense_example values (?,?,?);', (sid, text, type_))
         for nym in ('holonyms','meronyms','hypernyms','hyponyms','antonyms','synonyms','related'):
             for word in set(map(itemgetter('word'), sense.get(nym, []))):
-                cur.execute(f'insert into sense_{nym.rstrip("s")} values (?,?);', (sid, word))
+                cur.execute(f'insert or ignore into sense_{nym.rstrip("s")} values (?,?);', (sid, word))
 
 def _main():
     with sqlite3.connect(str(args.database)) as con:
@@ -45,14 +58,10 @@ def _main():
         with open(pkg_resources.resource_filename('vocabularium.res', 'schema.sql'), encoding='U8') as f:
             schema = f.read()
         cur.executescript(schema)
-        w, _ = os.get_terminal_size()
-        t0 = time_ns()
-        for i,x in enumerate(map(json.loads, args.input)):
-            txt = f'\r{i:,} {x["word"][-20:]}'
-            dur = f'{(time_ns() - t0) / 1e9:.2f}s'
-            print(txt + ' ' * (w - len(txt) - len(dur)) + dur, end='', flush=True)
+        for i, x in enumerate(map(json.loads, args.input)):
+            if args.verbose:
+                sys.stderr.write(f'\r{i+1:,} {x["word"][-20:]:20}')
             _insert_word(cur, x)
-        con.commit()
 
 if __name__ == '__main__':
     _main()
--- vocabularium/res/schema.sql
+++ vocabularium/res/schema.sql
@@ -1,74 +1,80 @@
 begin;
 create table if not exists word (
+    id integer primary key,
     lang_code text not null,
     word text not null,
-    primary key (lang_code, word)
-) without rowid;
+    unique (lang_code, word)
+);
 create table if not exists etymology (
-    lang_code text not null,
-    word text not null,
-    number number,
+    id integer primary key,
+    word_id text references word(id) on delete cascade,
+    number number not null,
     text text,
-    foreign key (lang_code, word) references word(lang_code, word) on delete cascade
+    unique (word_id, number)
 );
 create table if not exists etymology_template (
-    etymology_id references etymology(rowid) on delete cascade,
+    id integer primary key,
+    etymology_id references etymology(id) on delete cascade,
     expansion text not null,
-    name text not null
+    name text not null,
+    unique (etymology_id, expansion, name)
 );
 create table if not exists template_arg (
-    template_id references etymology_template(rowid) on delete cascade,
+    template_id references etymology_template(id) on delete cascade,
     key text not null,
     value text not null,
     primary key (template_id, key)
 ) without rowid;
 create table if not exists sense (
-    etymology_id references etymology(rowid) on delete cascade,
-    pos text
+    id integer primary key,
+    etymology_id references etymology(id) on delete cascade,
+    wiki_id text not null,
+    pos text,
+    unique (etymology_id, wiki_id)
 );
 create table if not exists sense_gloss (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     gloss text not null,
     primary key(sense_id, gloss)
 ) without rowid;
 create table if not exists sense_example (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     example text not null,
     type text,
     primary key(sense_id, example)
 ) without rowid;
 create table if not exists sense_holonym (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     word text not null,
     primary key(sense_id, word)
 ) without rowid;
 create table if not exists sense_meronym (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     word text not null,
     primary key(sense_id, word)
 ) without rowid;
 create table if not exists sense_hypernym (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     word text not null,
     primary key(sense_id, word)
 ) without rowid;
 create table if not exists sense_hyponym (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     word text not null,
     primary key(sense_id, word)
 ) without rowid;
 create table if not exists sense_antonym (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     word text not null,
     primary key(sense_id, word)
 ) without rowid;
 create table if not exists sense_synonym (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     word text not null,
     primary key(sense_id, word)
 ) without rowid;
 create table if not exists sense_related (
-    sense_id references sense(rowid) on delete cascade,
+    sense_id references sense(id) on delete cascade,
     word text not null,
     primary key(sense_id, word)
 ) without rowid;
